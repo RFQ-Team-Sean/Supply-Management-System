f3d110849fafcc5b761e4543f9b10193dc823621
SkCray Ace <skcrayace2525@gmail.com>
2025-01-15 21:16:09 +0800

    Revert "Sidebar Fix for Updated Role Names"
    This reverts commit a1191f33755cef0240a13926c48f29dce056861b.

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -226,7 +226,7 @@ export class SideBarComponent implements OnInit {
     const normalizedRole = role?.toLowerCase();
     
     switch (normalizedRole) {
-      case 'department staff':
+      case 'department':
         this.currentMenu = [...this.DepartmentuserMenu];
         this.title = 'Department Staff Portal'; // Set title for user menu
         break;
@@ -234,11 +234,11 @@ export class SideBarComponent implements OnInit {
         this.currentMenu = [...this.AdminMenu];
         this.title = 'Administrator Portal'; 
         break;
-      case 'gso officer':
+      case 'gso':
         this.currentMenu = [...this.GsoMenu];
         this.title = 'GSO Portal'; 
         break; 
-      case 'bac staff':
+      case 'bac':
         this.currentMenu = [...this.BacMenu];
         this.title = 'BAC Portal'; 
         break;    
